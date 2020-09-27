431778b6810b965cd347f9720aefe13bdd3bdd7d
D1M45210 <64825524+D1M45210@users.noreply.github.com>
2020-09-28 04:28:46 +0700

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+
+        <title>HACKED BY MeYousuE</title>
+
+<meta name="theme-color" content="#191919" /> 
+
+<meta property="og:title" content="- Who ? -" /> 
+
+<meta property="og:image" content="https://l.top4top.io/p_1651fgni80.jpg" />
+
+<meta property="og:description" content="[ MeYousuE ]" /> <link rel="shortcut icon" href="https://l.top4top.io/p_1651fgni80.jpg" type="image/x-icon" />
+
+<meta charset="utf-8" />
+
+<meta name="robots" content="all" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<link href="https://fonts.googleapis.com/css?family=Allan|Chakra+Petch|Iceland|Orbitron|Grenze" rel="stylesheet" /> 
+
+        <style type="text/css">
+
+        body{
+
+    background-color: #191919;
+
+    color : whitesmoke;
+
+    font-family: 'Grenze';
+
+        }
+
+      .mpsh{  
+
+  font-family: "Chakra Petch";
+
+  font-size: 20px;
+
+  color: whitesmoke ;
+
+         }
+
+         h3{
+
+  font-family: "Iceland";
+
+  font-size: 20px;
+
+  color: whitesmoke ;
+
+         }
+
+         h2{  
+
+  font-family: "Grenze";
+
+  font-size: 30px;
+
+  color: whitesmoke ;
+
+         }
+
+.loading{
+
+  display:flex;
+
+  justify-content:center;
+
+  align-items:center;
+
+  background-color:#f0f0ff;
+
+  color:#444499;
+
+  top:0;
+
+  transition: top 750ms;
+
+}
+
+.load{
+
+  display:flex;
+
+  justify-content:center;
+
+  align-items:center;
+
+  flex-direction:column;
+
+  opacity:0;
+
+  transition: opacity 2000ms;
+
+}
+
+body {
+
+  margin:0;
+
+}
+
+.layer{
+
+  width:100%;height:100%;
+
+  position:absolute;
+
+}
+
+.image{
+
+  width:150px;
+
+  height:150px;
+
+}
+
+.title{
+
+font-size: 45px;
+
+font-family: 'Iceland';
+
+color: #191919;
+
+font-weight:700;
+
+}
+
+                 </style>
+
+                 <script>
+
+onload = function () {
+
+   document.getElementsByClassName("load")[0].style.opacity = "1";
+
+   setTimeout(slideOut,7000);
+
+   function slideOut(){
+
+      document.getElementsByClassName("loading")[0].style.top = "-100vh";
+
+   }
+
+} 
+
+</script>
+
+             </head>
+
+             <body>
+
+             <center>
+
+        <div class="loading layer">
+
+          <div class="load">
+
+            <div class="image">
+
+              <img src="https://orig00.deviantart.net/df77/f/2013/094/8/d/loading_logofinal_by_zegerdon-d60eb1v.gif" width="150px" />
+
+            </div>
+
+            <div class="title">loading Bentar:v</div>
+
+          </div>
+
+        </div>
+
+<br />
+
+<audio id="lagunya"> <source src="https://s1.vocaroo.com/media/download_temp/Vocaroo_s1i9HU6v9Kyg.mp3" />  </audio>
+
+<img src="https://l.top4top.io/p_1651fgni80.jpg" height="210px" width="210px" />
+
+<h2>[ ! ] MeYousuE wus hir:v [ ! ]</h2>
+
+  : :   <img src="https://l.top4top.io/p_1651fgni80.jpg" width="30px" onclick="document.getElementById('lagunya').play();alert('If World Hate You'); alert('Dont Panic!');alert('Be Happy With Me'); alert('Dance, Laugh And Be Crazy'); alert('Lets Smile Today'); alert('Dont Forget Us'); alert('Ha Ha In Your System'); alert('hahaha terus pencet:v')" />   : :  
+
+<pre class="mpsh">
+
+___________  100%  ___________
+
+- MESSAGE -
+
+Remember admin, when you connect with
+
+computers are connected to every computer
+
+that is connected to the computer.
+
+___________  100%  ___________
+
+- PESAN -
+
+Ingat admin, ketika Anda terhubung dengan
+
+komputer lain, Anda terhubung ke setiap 
+
+komputer yang terhubung ke komputer.
+
+___________  100%  ___________
+
+- MY TEAM -
+
+.::<a href="http://blogparanoidcyber.blogspot.com">PARANOID CYBER TEAM</a>::.
+
+______________________________
+
+</pre>
